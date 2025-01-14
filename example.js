3e599258f323fa39fe0a8a92a334cc65db11aca0
DulanjaT <dulanja.coding@gmail.com>
2025-01-14 13:02:56 +0200

    added new build

--- example.js
+++ example.js
@@ -5,4 +5,4 @@ console.log(isNullOrEmpty("")); // true
 console.log(isNullOrEmpty(null)); // true
 console.log(isNullOrEmpty(undefined)); // true
 
-console.log(isNullOrEmpty("Hello World")); // false
+console.log(isNullOrEmpty("Hello World")); 
